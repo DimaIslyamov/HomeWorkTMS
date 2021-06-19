9204bed57b2de906256e7cf39a3a5fac53512cd4
dimaIslyamov <islyaamov.dimaa@gmail.com>
2021-06-19 21:10:23 +0200

    Create tasks 1-5 and 7-8 ( task 6 tomorrow ) =)

---
+++ homeWorkLesson3/homeWorkLesson3/New Group/Task7.swift
@@ -0,0 +1,177 @@
+//
+//  Task7.swift
+//  homeWorkLesson3
+//
+//  Created by Moobat on 19.06.21.
+//
+
+import Foundation
+import UIKit
+
+class Task7: UIViewController {
+    
+    typealias Students = (name: String, carNumber: Int?, testResult: Int?)
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        studentTask()
+    }
+    
+    func studentTask() {
+        
+        // MARK: Student Max
+        let studentMax: Students = ("Max", 3017, nil)
+        
+        var maxResult = "Имя студента: \(studentMax.name), "
+        
+        if studentMax.carNumber != nil {
+            maxResult += "машина: есть, "
+        } else {
+            maxResult += "машины: нету, "
+        }
+        
+        if studentMax.carNumber != nil {
+            maxResult += "номер машины: \(String(describing: studentMax.carNumber!)), "
+            } else {
+                maxResult += "номер машины нету, "
+            }
+        
+        if studentMax.testResult != nil {
+            maxResult += "на контрольной был "
+        } else {
+            maxResult += "не был на контрольной "
+        }
+        
+        if studentMax.testResult != nil {
+            maxResult += "оценка: \(String(describing: studentMax.testResult!)). "
+            } else {
+                maxResult += "оценка Колл 🤣."
+            }
+        
+        print(maxResult)
+        
+        // MARK: Student Dima
+        let studentDima: Students = ("Dima", nil, 8)
+        
+        var dimaResult = "Имя студента: \(studentDima.name), "
+
+        if studentDima.carNumber != nil {
+            dimaResult += "машина: есть, "
+        } else {
+            dimaResult += "машины: нету, "
+        }
+        
+        if studentDima.carNumber != nil {
+            dimaResult += "номер машины: \(String(describing: studentDima.carNumber!)), "
+            } else {
+                dimaResult += "номерa машины нету, "
+            }
+        
+        if studentDima.testResult != nil {
+            dimaResult += "на контрольной был "
+        } else {
+            dimaResult += "не был на контрольной "
+        }
+        
+        if studentDima.testResult != nil {
+            dimaResult += "оценка: \(String(describing: studentDima.testResult!)). "
+            } else {
+                dimaResult += "оценка Колл 🤣."
+            }
+        
+        print(dimaResult)
+        
+        // MARK: Student Misha
+        let studentMisha: Students = ("Misha", 2567, nil)
+        
+        var mishaResult = "Имя студента: \(studentMisha.name), "
+        
+        if studentMisha.carNumber != nil {
+            mishaResult += "машина: есть, "
+        } else {
+            mishaResult += "машины: нету, "
+        }
+        
+        if studentMisha.carNumber != nil {
+            mishaResult += "номер машины: \(String(describing: studentMisha.carNumber!)), "
+            } else {
+                mishaResult += "номерa машины нету, "
+            }
+        
+        if studentMisha.testResult != nil {
+            mishaResult += "на контрольной был "
+        } else {
+            mishaResult += "не был на контрольной "
+        }
+        
+        if studentMisha.testResult != nil {
+            mishaResult += "оценка: \(String(describing: studentMisha.testResult!)). "
+            } else {
+                mishaResult += "оценка Колл 🤣."
+            }
+        
+        print(mishaResult)
+
+        // MARK: Student Oleg
+        let studentOleg: Students = ("Oleg", 1777, 5)
+        
+        var olerResult = "Имя студента: \(studentOleg.name), "
+
+        if studentOleg.carNumber != nil {
+            olerResult += "машина: есть, "
+        } else {
+            olerResult += "машины: нету, "
+        }
+        
+        if studentOleg.carNumber != nil {
+            olerResult += "номер машины: \(String(describing: studentOleg.carNumber!)), "
+            } else {
+                olerResult += "номерa машины нету, "
+            }
+        
+        if studentOleg.testResult != nil {
+            olerResult += "на контрольной был "
+        } else {
+            olerResult += "не был на контрольной "
+        }
+        
+        if studentOleg.testResult != nil {
+            olerResult += "оценка: \(String(describing: studentOleg.testResult!)). "
+            } else {
+                olerResult += "оценка Колл 🤣."
+            }
+        
+        print(olerResult)
+        
+        // MARK: Student Anjey
+        let studentAnjey: Students = ("Anjey", nil, 2)
+        
+        var anjeyResult = "Имя студента: \(studentAnjey.name), "
+        
+        if studentAnjey.carNumber != nil {
+            anjeyResult += "машина: есть, "
+        } else {
+            anjeyResult += "машины: нету, "
+        }
+        
+        if studentAnjey.carNumber != nil {
+            anjeyResult += "номер машины: \(String(describing: studentAnjey.carNumber!)), "
+            } else {
+                anjeyResult += "номерa машины нету, "
+            }
+        
+        if studentAnjey.testResult != nil {
+            anjeyResult += "на контрольной был "
+        } else {
+            anjeyResult += "не был на контрольной "
+        }
+        
+        if studentAnjey.testResult != nil {
+            anjeyResult += "оценка: \(String(describing: studentAnjey.testResult!)). "
+            } else {
+                anjeyResult += "оценка Колл 🤣."
+            }
+        
+        print(anjeyResult)
+    }
+}
---
+++ homeWorkLesson3/homeWorkLesson3/New Group/ViewController.swift
@@ -0,0 +1,161 @@
+//
+//  ViewController.swift
+//  homeWorkLesson3
+//
+//  Created by Moobat on 18.06.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    typealias MyAchievements = (pushUps: Int, pullUps: Int, squats: Int)
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        butt()
+        achievements()
+        errorTask6()
+        task5()
+        errorTask6()
+        task8()
+    }
+    
+
+    // MARK: - Tasks 1 2 3 4
+    
+    func achievements() {
+        // создание кортежа 1
+        var myAchievements: MyAchievements = (90, 50, 65)
+        print(myAchievements)
+        // принт индекса и значения
+        print("Значения по параметру pushUps: \(myAchievements.pushUps), pullUps: \(myAchievements.pullUps), squats: \(myAchievements.squats)")
+        print("Значения по индексу pushUps: \(myAchievements.0), pullUps: \(myAchievements.1), squats: \(myAchievements.2)")
+        
+        // кортеж 2
+        var igorAchievements: MyAchievements = (80, 20, 50)
+        // использование промежуточной переменной
+        let intermediateVar = myAchievements
+        myAchievements = igorAchievements
+        igorAchievements = intermediateVar
+        
+        // создание кортежа 3 и присваивание разницы
+        let antonAchievements = (pushUps: (igorAchievements.pushUps - myAchievements.pushUps), pullUps: (igorAchievements.pullUps - myAchievements.pullUps), squats:(igorAchievements.squats - myAchievements.squats))
+        // принт кортежа 3
+        print("Достижения Антона: pushUps: \(antonAchievements.pushUps), pullUps: \(antonAchievements.pullUps), squats: \(antonAchievements.squats)")
+    }
+
+    // MARK: - TASK 5
+
+    func task5() {
+        let song: String = "17"
+        let album: String = "5"
+        let cash: String = "35baks"
+        let stars: String = "89"
+        let age: String = "7age"
+        // переменная с суммами всеx констант
+        // nil-coalescing
+        var sum = (Int(cash) ?? 0) + (Int(age) ?? 0)
+        
+        let cashCon = Int(cash)
+        let ageCon = Int(age)
+        
+        // optional binding
+        let songCon = Int(song)
+        if let song = songCon {
+            sum = sum + song
+        }
+        
+        let starsCon = Int(stars)
+        if let stars = starsCon {
+            sum = sum + stars
+        }
+        // forced unwrapping
+        let albumCon = Int(album)
+        if albumCon != nil {
+            sum += albumCon!
+        }
+        
+        let string = "\(songCon!) + \(starsCon!) + \(String(describing: cashCon)) + \(albumCon!) + \(String(describing: ageCon)) = \(sum)"
+        print(string)
+    }
+    
+    
+    // MARK: - Task 6
+    
+    func errorTask6() {
+        
+    }
+    
+    // MARK: - Task 8
+    
+    func task8() {
+        let intTuple: (int1: Int?, int2: Int?, int3: Int?, int4: Int?, int5: Int?) = (25, 3, 56, 11, 78)
+        // optional binding
+        var sumOptionalBinding = 0
+        if let int1 = intTuple.int1 {
+            sumOptionalBinding = sumOptionalBinding + int1
+            }
+        if let int2 = intTuple.int2 {
+            sumOptionalBinding = sumOptionalBinding + int2
+        }
+        if let int3 = intTuple.int3 {
+            sumOptionalBinding = sumOptionalBinding + int3
+        }
+        if let int4 = intTuple.int4 {
+            sumOptionalBinding = sumOptionalBinding + int4
+        }
+        if let int5 = intTuple.int5 {
+            sumOptionalBinding = sumOptionalBinding + int5
+        }
+        print("Optional binding sum: \(sumOptionalBinding)")
+        
+        // forced unwrapping
+        var sumForcedUnwrapping = 0
+        if intTuple.int1 != nil {
+            sumForcedUnwrapping += intTuple.int1!
+        }
+        if intTuple.int2 != nil {
+            sumForcedUnwrapping += intTuple.int2!
+        }
+        if intTuple.int3 != nil {
+            sumForcedUnwrapping += intTuple.int3!
+        }
+        if intTuple.int4 != nil {
+            sumForcedUnwrapping += intTuple.int4!
+        }
+        if intTuple.int5 != nil {
+            sumForcedUnwrapping += intTuple.int5!
+        }
+        print("Forsed unwrapping sum: \(sumForcedUnwrapping)")
+        
+        // nil-coalescing
+        var sumNilCoalecing = (intTuple.int1 ?? 0) + (intTuple.int2 ?? 0) + (intTuple.int3 ?? 0)
+        sumNilCoalecing += (intTuple.int4 ?? 0) + (intTuple.int5 ?? 0)
+        print("nil-coalescing sum : \(sumNilCoalecing)")
+    }
+}
+
+
+// MARK: Extension for Task 7
+extension ViewController {
+    func butt() {
+    let button = UIButton(frame: CGRect(x: 0, y: 0, width: 200, height: 50))
+        view.addSubview(button)
+        button.backgroundColor = .systemBlue
+        button.layer.cornerRadius = 10
+        button.setTitle("Task 7", for: .normal)
+        button.center = view.center
+        button.setTitleColor(.white, for: .normal)
+        button.addTarget(self, action: #selector(showTask7), for: .touchUpInside)
+    }
+    
+    @objc private func showTask7() {
+        let storyBoard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+        let task7 = storyBoard.instantiateViewController(identifier: "task77") as! Task7
+        self.present(task7, animated: true, completion: nil)
+    }
+}
+
+
+
