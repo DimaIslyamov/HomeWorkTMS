8534990b2d6a723410160c5d28ce54f9c3c56058
dimaIslyamov <islyaamov.dimaa@gmail.com>
2021-08-03 23:43:32 +0200

    fix chessBoard

--- homeWorkLesson10(12, 13, 14, 15)/homeWorkLesson10/Controllers/SettingsViewController.swift
+++ homeWorkLesson10(12, 13, 14, 15)/homeWorkLesson10/Controllers/SettingsViewController.swift
@@ -9,34 +9,28 @@ import UIKit
 
 class SettingsViewController: UIViewController {
     
-    @IBOutlet  var buttonViews: [UIView]!
-    @IBOutlet  var buttonOutlet: [UIButton]!
+    @IBOutlet weak var buttonViews: UIView!
+    @IBOutlet weak var buttonOutlet: UIButton!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         
-        buttonViews.forEach { can in
-            can.layer.cornerRadius = 12
-            can.layer.shadowColor = UIColor.black.cgColor
-            can.layer.shadowRadius = 4
-            can.layer.shadowOpacity = 0.9
-            can.layer.shadowOffset = CGSize(width: 5, height: 5)
-        }
+        buttonViews.layer.cornerRadius = 12
+        buttonViews.layer.shadowColor = UIColor.black.cgColor
+        buttonViews.layer.shadowRadius = 4
+        buttonViews.layer.shadowOpacity = 0.9
+        buttonViews.layer.shadowOffset = CGSize(width: 5, height: 5)
         
-        buttonOutlet.forEach { can in
-            can.layer.borderWidth = 2
-            can.layer.borderColor = UIColor.black.cgColor
-            can.layer.cornerRadius = 12
-        }
+        buttonOutlet.layer.borderWidth = 2
+        buttonOutlet.layer.borderColor = UIColor.black.cgColor
+        buttonOutlet.layer.cornerRadius = 12
+        
     }
     
     @IBAction func backButtonAction(_ sender: UIButton) {
         navigationController?.popViewController(animated: true)
     }
     
-    @IBAction func aboutButtonTapped(_ sender: UIButton) {
-        
-    }
 }
