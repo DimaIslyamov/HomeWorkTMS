7e7d055060435ac67aacab4d92888bb61a86674f
dimaIslyamov <islyaamov.dimaa@gmail.com>
2021-08-04 22:09:56 +0200

    сделал scrollview(но не очень)

--- homeWorkLesson10(12, 13, 14, 15)/homeWorkLesson10/Controllers/AboutViewController.swift
+++ homeWorkLesson10(12, 13, 14, 15)/homeWorkLesson10/Controllers/AboutViewController.swift
@@ -9,13 +9,29 @@ import UIKit
 
 class AboutViewController: UIViewController {
 
+    @IBOutlet weak var buttonView: UIView!
+    @IBOutlet weak var backButtonOutlet: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         
+        buttonView.layer.cornerRadius = 12
+        
+        backButtonOutlet.layer.borderWidth = 2
+        backButtonOutlet.layer.borderColor = UIColor.black.cgColor
+        backButtonOutlet.layer.cornerRadius = 12
+        
+        buttonView.layer.shadowColor = UIColor.black.cgColor
+        buttonView.layer.shadowRadius = 4
+        buttonView.layer.shadowOpacity = 0.9
+        buttonView.layer.shadowOffset = CGSize(width: 5, height: 5)
+        
     }
     
-
+    @IBAction func backButtonAction(_ sender: UIButton) {
+        navigationController?.popViewController(animated: true)
+    }
     
 
 }
