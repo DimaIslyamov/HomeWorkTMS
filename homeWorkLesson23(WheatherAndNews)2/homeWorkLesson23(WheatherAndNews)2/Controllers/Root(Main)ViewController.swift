2012d5d8fdca4156c9c24f2083e3c5b716930b1c
dimaIslyamov <islyaamov.dimaa@gmail.com>
2021-09-19 18:01:29 +0200

    в приложение погоды добавил музыку и видео(выполнил дз25)

--- homeWorkLesson23(WheatherAndNews)2/homeWorkLesson23(WheatherAndNews)2/Controllers/Root(Main)ViewController.swift
+++ homeWorkLesson23(WheatherAndNews)2/homeWorkLesson23(WheatherAndNews)2/Controllers/Root(Main)ViewController.swift
@@ -7,10 +7,12 @@
 
 import UIKit
 import SSBouncyButton
+import AVFoundation
+import AVKit
 
 class WheathreViewController: UIViewController {
     
-    @IBOutlet weak var backgroundImageView: UIImageView!
+    @IBOutlet weak var videoView: UIView!
     @IBOutlet weak var wheathreIconImage: UIImageView!
     @IBOutlet weak var temperatureLable: UILabel!
     @IBOutlet weak var feelsLike: UILabel!
@@ -20,7 +22,8 @@ class WheathreViewController: UIViewController {
     @IBOutlet weak var miniSearchButton: UIView!
     
     var followButton = SSBouncyButton()
-    
+    var player: AVPlayer?
+    var playerMusic: AVPlayer?
     
     var city = ""
     
@@ -48,9 +51,56 @@ class WheathreViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         settuoForSSButton()
-        view.addSubview(setBackground(with: "WheathreBackground"))
+        setBackgraundVideoView()
+        setBackgroundMusic()
     }
     
+    
+    func setBackgroundMusic() {
+        guard let pathMusic = Bundle.main.path(forResource: "zvuki-prirody", ofType: "mp3") else { return }
+        
+        let urlMusic = URL(fileURLWithPath: pathMusic)
+        let asset = AVAsset(url: urlMusic)
+        let playerItem = AVPlayerItem(asset: asset)
+        let player = AVPlayer(playerItem: playerItem)
+        
+        self.playerMusic = player
+        player.play()
+    }
+    
+    
+    func setBackgraundVideoView() {
+        guard let pathVideo = Bundle.main.path(forResource: "Rain - 20885", ofType: "mp4") else { return }
+        
+        NotificationCenter.default.removeObserver(self)
+        
+        let urlVideo = URL(fileURLWithPath: pathVideo)
+        let asset = AVAsset(url: urlVideo)
+        let playerItem = AVPlayerItem(asset: asset)
+        let player = AVPlayer(playerItem: playerItem)
+        
+        self.player = player
+        
+        let videoLayer = AVPlayerLayer(player: player)
+        videoLayer.frame = CGRect(origin: .zero, size: videoView.frame.size)
+        videoLayer.videoGravity = .resizeAspectFill
+        videoView.layer.addSublayer(videoLayer)
+        self.player?.play()
+        NotificationCenter.default.addObserver(forName: .AVPlayerItemDidPlayToEndTime, object: playerItem, queue: .main) { _ in
+            print("AVPlayer Item Did Play To End Time")
+            self.player?.seek(to: .zero)
+            self.player?.play()
+        }
+    }
+    
+    
+    @IBAction func switchOffOn(_ sender: UISwitch) {
+        if sender.isOn {
+            self.playerMusic?.play()
+        } else {
+            self.playerMusic?.pause()
+        }
+    }
     
     
     @objc func buttonDidPress(_ sender: UIButton) {
